aec1a66adf329316b62fa373d408e319e46f008d
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-04-28 15:11:51 -0700

    Adding product list implementation

--- tracing-demo-gateway/src/main/java/com/github/ajanthan/tracing/spring/gw/tracingdemogateway/TracingDemoGatewayApplication.java
+++ tracing-demo-gateway/src/main/java/com/github/ajanthan/tracing/spring/gw/tracingdemogateway/TracingDemoGatewayApplication.java
@@ -2,11 +2,13 @@ package com.github.ajanthan.tracing.spring.gw.tracingdemogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class TracingDemoGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TracingDemoGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TracingDemoGatewayApplication.class, args);
+    }
 }
