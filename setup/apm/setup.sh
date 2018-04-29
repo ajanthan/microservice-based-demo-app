46eb7ee292ae090ea7fe28695864432f5ef82e3a
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-04-29 00:42:52 -0700

    fixing rsync issue

--- setup/apm/setup.sh
+++ setup/apm/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-apm_version=5.0.0-alpha
-
-unzip apache-skywalking-apm-incubating-$apm_version.zip -d working
-rsync -avz --exclude apache-skywalking-apm-incubating/config apache-skywalking-apm-incubating/agent ../agents/business-service
-rsync -avz --exclude apache-skywalking-apm-incubating/config apache-skywalking-apm-incubating/agent ../agents/data-service
-rsync -avz --exclude apache-skywalking-apm-incubating/config apache-skywalking-apm-incubating/agent ../agents/gateway
-rsync -avz --exclude apache-skywalking-apm-incubating/config apache-skywalking-apm-incubating/agent ../agents/webui
+apm_version=${version:=5.0.0-alpha}
+mkdir -p working
+tar -xvzf apache-skywalking-apm-incubating-$apm_version.tar.gz -C working
+rsync -avz --exclude config working/apache-skywalking-apm-incubating/agent/ ../agents/business-service
+rsync -avz --exclude config working/apache-skywalking-apm-incubating/agent/ ../agents/data-service
+rsync -avz --exclude config working/apache-skywalking-apm-incubating/agent/ ../agents/gateway
+rsync -avz --exclude config working/apache-skywalking-apm-incubating/agent/ ../agents/webui
