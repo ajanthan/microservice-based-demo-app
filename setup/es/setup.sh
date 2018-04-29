ced020a02b14e91c2aaf4ab6457f66ef46987298
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-04-29 00:16:17 -0700

    fixing copy directory

--- setup/es/setup.sh
+++ setup/es/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 es_version=5.5.0
 unzip elasticsearch-$es_version.zip -d working
-rsync -avz  elasticsearch.yml elasticsearch-$es_version/config
+rsync -avz  elasticsearch.yml working/elasticsearch-$es_version/config
